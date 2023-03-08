2654c7a6552745c25ccdb587361f5c8343b55e42
BeepSheep254 <113206839+BeepSheep254@users.noreply.github.com>
2023-03-08 18:19:03 +0000

    fi

--- source/BootingUp.hx
+++ source/BootingUp.hx
@@ -52,27 +52,30 @@ import Discord.DiscordClient;
 
 using StringTools;
 
-class BootingUp extends MusicBeatState {
+class BootingUp extends MusicBeatState
+{
 	public var logoAnim:FlxSprite;
 	public var loadingMush:FlxSprite;
 	public var loadedTxt:FlxFixedText;
 	public var loadedImages:Int = 0;
 	public var loadBar:FlxBar;
 	public var loadBarSpr:FlxSprite;
+
 	var boolOne:Bool = false;
 	var boolTwo:Bool = false;
 	var iscompleted:Bool = false;
 	var fuck:Bool = false;
 
-	override function create() {
-        #if LUA_ALLOWED
+	override function create()
+	{
+		#if LUA_ALLOWED
 		Paths.pushGlobalMods();
 		#end
 
 		FlxG.save.bind('funkin', CoolUtil.getSavePath());
-		//PlayerSettings.init();
+		// PlayerSettings.init();
 
-        Paths.clearStoredMemory();
+		Paths.clearStoredMemory();
 		Paths.clearUnusedMemory();
 
 		FlxG.game.focusLostFramerate = 60;
@@ -89,12 +92,12 @@ class BootingUp extends MusicBeatState {
 		Highscore.load();
 
 		loadingMush = new FlxSprite().loadGraphic(Paths.image("foxaCacheBG"));
-		loadingMush.scale.set(0.4,0.4);
+		loadingMush.scale.set(0.4, 0.4);
 		loadingMush.screenCenter();
 		add(loadingMush);
 
-        loadingMush = new FlxSprite().loadGraphic(Paths.image("foxaEngineLogo"));
-		logoAnim.scale.set(0.4,0.4);
+		loadingMush = new FlxSprite().loadGraphic(Paths.image("foxaEngineLogo"));
+		logoAnim.scale.set(0.4, 0.4);
 		logoAnim.screenCenter();
 		logoAnim.y += 500;
 		logoAnim.x += 500;
@@ -108,48 +111,51 @@ class BootingUp extends MusicBeatState {
 		loadedTxt.y -= 100;
 		add(loadedTxt);
 
-		loadBar = new FlxBar(0,0,LEFT_TO_RIGHT,Std.int(1110 / 2),15,this,'loadedImages',0,247,true);
+		loadBar = new FlxBar(0, 0, LEFT_TO_RIGHT, Std.int(1110 / 2), 15, this, 'loadedImages', 0, 247, true);
 		// loadBar.createGradientBar([FlxColor.BLACK, FlxColor.GRAY], [FlxColor.ORANGE, FlxColor.YELLOW],1,180,true,FlxColor.BLACK);
 		loadBar.createFilledBar(FlxColor.BLACK, FlxColor.WHITE, true, FlxColor.BLACK);
 		loadBar.antialiasing = true;
 		loadBar.numDivisions = 10000;
 		loadBar.screenCenter();
-        add(loadBar);
+		add(loadBar);
 
 		// FlxG.sound.playMusic(Paths.music('freakyMenu'), 0);//genius momento. agree
 
 		loadedTxt.text = "PRELOADED ASSETS: 0";
 		fuck = true;
-				#if cpp
-				Thread.create(cacheStuff);
-				#else
-				cacheStuff();
-				#end
-		else
-			iscompleted = true;
+		#if cpp
+		Thread.create(cacheStuff);
+		#else
+		cacheStuff();
+		#end
 
-		//trace(SysPathing.getExePath("do NOT readme.txt"));
+		trace('caching images and sound...');
 
 		super.create();
 	}
-	
-	override public function update(h:Float) {
+
+	override public function update(h:Float)
+	{
 		super.update(h);
 
-		if (iscompleted) { //  && boolOne && boolTwo
-				loadedTxt.text = "Assets loaded!\nPress ENTER to continue.\n";
-			if (FlxG.keys.justPressed.ENTER) {
+		if (iscompleted)
+		{ //  && boolOne && boolTwo
+			loadedTxt.text = "Assets loaded!\nPress ENTER to continue.\n";
+			if (FlxG.keys.justPressed.ENTER)
+			{
 				MusicBeatState.switchState(new TitleState());
 				trace(loadedImages);
 			}
 		}
-		if (loadedTxt != null && fuck && !iscompleted) {
+		if (loadedTxt != null && fuck && !iscompleted)
+		{
 			loadedTxt.text = "LOADED ASSETS: " + loadedImages;
 		}
 	}
 
-	//own made caching :P -zackk
-	function cacheStuff() { //very hard goin, keep preloading assets only to big asf shit like ui and dialogue.
+	// own made caching :P -zackk
+	function cacheStuff()
+	{ // very hard goin, keep preloading assets only to big asf shit like ui and dialogue.
 		#if sys
 		loadfolder("assets/secrets/images");
 		loadfolder("assets/week7/images");
@@ -171,17 +177,18 @@ class BootingUp extends MusicBeatState {
 		loadfolder("assets/shared/images/dialogue");
 		FlxG.sound.cacheAll();
 		#end
-        trace('cache is done looks like we are readuy');
+		trace('cache is done looks like we are readuy');
 		iscompleted = true;
 	}
 
-	function loadfolder(folder:String) {
+	function loadfolder(folder:String)
+	{
 		var charpaths = FileSystem.readDirectory(Sys.getCwd() + '/' + folder);
 
 		for (path in charpaths)
 		{
 			var fullpath = '$folder/$path';
-			//trace(fullpath);
+			// trace(fullpath);
 
 			if (!path.contains('.png') || !FileSystem.exists(Sys.getCwd() + '/' + fullpath))
 				continue;
@@ -197,7 +204,7 @@ class BootingUp extends MusicBeatState {
 
 			Paths.cachedAsses.set(fullpath, stupi);
 			Paths.cachedShit.push(fullpath);
-			//stupi.destroy();
+			// stupi.destroy();
 			// since its all in cached, we dont need cache anymore?? idfk.
 			Paths.cache.clear(fullpath);
 			if (Paths.cache.hasBitmapData(fullpath))
